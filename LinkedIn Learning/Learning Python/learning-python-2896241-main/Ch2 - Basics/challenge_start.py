cc3d769a6904aea395344cfc6a518e2069396f52
CeraStroh <strohc1@central.edu>
2023-04-12 20:42:35 -0500

    Update challenge_start.py

--- LinkedIn Learning/Learning Python/learning-python-2896241-main/Ch2 - Basics/challenge_start.py
+++ LinkedIn Learning/Learning Python/learning-python-2896241-main/Ch2 - Basics/challenge_start.py
@@ -1,17 +1,20 @@
 def main():
-    word = input("Enter string to test for palindrome or 'exit':")
-    test = ""
-    if word == "exit":
-        exit()
-    elif word.isalnum:
-        print("num")
-    else:
-        for w in word:
-            test += w
-        if test == word:
-            print("Palindrome test: True")
+    run = True
+    
+    while (run):
+        word = input("Enter string to test for palindrome or 'exit':")
+        test = ""
+        if word == "exit":
+            run = False
+        elif word.isalnum():
+            print("num")
         else:
-            print("Palindrome test: False")
+            for w in word:
+                test += w
+            if test == test[::-1]:
+                print("Palindrome test: True")
+            else:
+                print("Palindrome test: False")
 
 if __name__ == "__main__":
     main()
