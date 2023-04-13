339ca5d16536b3440dbd4c429b527097259a5ffb
CeraStroh <strohc1@central.edu>
2023-04-12 20:33:34 -0500

    Update files_start.py

--- LinkedIn Learning/Learning Python/learning-python-2896241-main/Ch3 - Files/files_start.py
+++ LinkedIn Learning/Learning Python/learning-python-2896241-main/Ch3 - Files/files_start.py
@@ -6,19 +6,26 @@
 
 def main():  
     # Open a file for writing and create it if it doesn't exist
-
+    # myFile = open("textfile", "w+")
     
     # Open the file for appending text to the end
-
+    # myFile = open("textfile", "a+")
 
     # write some lines of data to the file
-
+    # for i in range(10):
+    #     myFile.write("This is some new text\n")
     
     # close the file when done
-
+    # myFile.close()
     
     # Open the file back up and read the contents
-
+    myFile = open("textfile", "r")
+    if myFile.mode == 'r':
+        # contents = myFile.read()
+        # print(contents)
+        fileLine = myFile.readlines()
+        for x in fileLine:
+            print(x)
     
 if __name__ == "__main__":
     main()
